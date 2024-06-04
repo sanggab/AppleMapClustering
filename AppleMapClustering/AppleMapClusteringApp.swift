7d80d0c5e2cd49216634c7ba13fbf48b966a7ee3
심상갑 <kappa2410@gmail.com>
2024-06-04 09:39:15 +0900

    Initial Commit

---
+++ AppleMapClustering/AppleMapClusteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMapClusteringApp.swift
+//  AppleMapClustering
+//
+//  Created by Gab on 2024/06/04.
+//
+
+import SwiftUI
+
+@main
+struct AppleMapClusteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMapClustering/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleMapClustering
+//
+//  Created by Gab on 2024/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
