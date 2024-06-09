e2ec4fe73fdc58ee28e9eeaa5b5549d20779b43d
심상갑 <kappa2410@gmail.com>
2024-06-10 08:57:53 +0900

    Feat: 수정중

--- AppleMapClustering/ContentView.swift
+++ AppleMapClustering/ContentView.swift
@@ -24,20 +24,6 @@ struct ContentView: View {
                     }
                     .padding(.bottom, 30)
             }
-//            .onAppear {
-//                AF.request(URL(string: "https://devapi-nadiu12.honeymatchs.com/api/v2/map/test/600")!, method: .post)
-//                    .responseDecodable(of: [Person].self) { response in
-//                        switch response.result {
-//                        case .success(let data):
-//                            mapData = data
-//                        case .failure(let error):
-//                            print("error : \(error)")
-//                        }
-//                    }
-//            }
-//            .onChange(of: mapData) { newValue in
-//                print("mapData count : \(newValue.count)")
-//            }
     }
 }
 
